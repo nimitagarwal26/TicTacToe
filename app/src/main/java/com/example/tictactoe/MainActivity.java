43a88854b3a8d2b2cc48de6e404d913f9176fa5b
nimitagarwal <101705195+agarwalnimit@users.noreply.github.com>
2023-05-12 17:16:48 +0530

    cross and circle added

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,8 +3,22 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    int player=1;
+    public void load(View view){
+        ImageView v=(ImageView) view;
+        if(player==1) {
+            v.setImageResource(R.drawable.cross);
+            player=0;
+        }
+        else{
+            v.setImageResource(R.drawable.circle);
+            player=1;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

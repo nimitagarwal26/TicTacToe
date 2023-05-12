4e8403938a07451ba7369f78e3b5b6fc4785e017
nimitagarwal <101705195+agarwalnimit@users.noreply.github.com>
2023-05-12 21:47:44 +0530

    bugs edited and reset button added

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,38 +1,58 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isWinner=false;
+    int imageClicked=-1;
     int player=1;
     int [][]winningStates={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     int []gameState={-1,-1,-1,-1,-1,-1,-1,-1,-1};
     public void load(View view){
-        ImageView v=(ImageView) view;
-        int tag=Integer.parseInt(v.getTag().toString());
-        if(player==1) {
-            v.setImageResource(R.drawable.cross);
-            gameState[tag]=player;
-            Toast.makeText(this,tag+""+" Cross",Toast.LENGTH_SHORT).show();
-            player=0;
-        }
-        else{
-            v.setImageResource(R.drawable.circle);
-            gameState[tag]=player;
-            Toast.makeText(this,tag+""+" Zero",Toast.LENGTH_SHORT).show();
-            player=1;
-        }
-        for (int[] winningState : winningStates) {
-            if (gameState[winningState[0]] == gameState[winningState[1]] && gameState[winningState[1]] == gameState[winningState[2]] && gameState[winningState[0]] > -1) {
-                Toast.makeText(this, "Winner is " + (player == 0 ? 1 : 0), Toast.LENGTH_SHORT).show();
+        ImageView v = (ImageView) view;
+        int tag = Integer.parseInt(v.getTag().toString());
+        imageClicked=gameState[tag];
+        if(isWinner==false && imageClicked==-1) {
+            if (player == 1) {
+                v.setImageResource(R.drawable.cross);
+                gameState[tag] = player;
+                Toast.makeText(this, tag + "" + " Cross",Toast.LENGTH_SHORT).show();
+                player = 0;
+            } else {
+                v.setImageResource(R.drawable.circle);
+                gameState[tag] = player;
+                Toast.makeText(this, tag + "" + " Zero", Toast.LENGTH_SHORT).show();
+                player = 1;
+            }
+            for (int[] winningState : winningStates) {
+                if (gameState[winningState[0]] == gameState[winningState[1]] && gameState[winningState[1]] == gameState[winningState[2]] && gameState[winningState[0]] > -1) {
+                    Toast.makeText(this, "Winner is " + (player == 0 ? 1 : 0), Toast.LENGTH_SHORT).show();
+                    isWinner=true;
+                }
             }
         }
     }
-
+    public void reset(View view){
+        GridLayout gridLayout=findViewById(R.id.gridLayout);
+        int total_images=gridLayout.getChildCount();
+        for(int i=0;i<total_images;i++){
+            ImageView v=(ImageView)gridLayout.getChildAt(i);
+            v.setImageDrawable(null);
+        }
+        isWinner=false;
+        imageClicked=-1;
+        player=1;
+        for(int i=0;i<gameState.length;i++){
+            gameState[i]=-1;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

1021c188062ed4ba3aaf4f1ed5f9bff0e25bf8aa
nimitagarwal <101705195+agarwalnimit@users.noreply.github.com>
2023-05-12 17:30:43 +0530

    Toast for tags added

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int player=1;
     public void load(View view){
         ImageView v=(ImageView) view;
+        int tag=Integer.parseInt(v.getTag().toString());
         if(player==1) {
             v.setImageResource(R.drawable.cross);
+            Toast.makeText(this,tag+""+" Cross",Toast.LENGTH_SHORT).show();
             player=0;
         }
         else{
             v.setImageResource(R.drawable.circle);
+            Toast.makeText(this,tag+""+" Zero",Toast.LENGTH_SHORT).show();
             player=1;
         }
     }
